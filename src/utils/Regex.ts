6440f8ccf40fa1f4ce6ad334f022f8752c4f5ca0
Martin Wechtitsch <martin.wechtitsch@optimatik.ch>
2025-10-24 14:44:59 +0200

    enhance NextIntl integration to handle multiple scopes within one file

--- src/utils/Regex.ts
+++ src/utils/Regex.ts
@@ -1,29 +1,37 @@
 import { sortBy } from 'lodash'
-import { QUOTE_SYMBOLS } from '../meta'
+import { Config, CurrentFile } from '~/core'
+import { isNextIntlScopeRange, NextIntlScopeRange } from '~/frameworks/next-intl'
+import i18n from '~/i18n'
+import { Log } from '.'
 import { KeyInDocument, RewriteKeyContext } from '../core/types'
 import { ScopeRange } from '../frameworks/base'
-import { Log } from '.'
-import i18n from '~/i18n'
-import { CurrentFile, Config } from '~/core'
+import { QUOTE_SYMBOLS } from '../meta'
 
 export function handleRegexMatch(
   text: string,
   match: RegExpExecArray,
   dotEnding = false,
   rewriteContext?: RewriteKeyContext,
-  scopes: ScopeRange[] = [],
+  scopes: ScopeRange[] | NextIntlScopeRange[] = [],
   namespaceDelimiters = [':', '/'],
   defaultNamespace?: string,
   starts: number[] = [],
 ): KeyInDocument | undefined {
   const matchString = match[0]
-  let key = match[1]
+
+  let keyIndex = 1
+  let nextIntlFunctionName = undefined
+  if (scopes.some(s => isNextIntlScopeRange(s))) {
+    keyIndex = 2
+    nextIntlFunctionName = match[1]
+  }
+  let key = match[keyIndex]
   if (!key)
     return
 
   const start = match.index + matchString.lastIndexOf(key)
   const end = start + key.length
-  const scope = scopes.find(s => s.start <= start && s.end >= end)
+  const scope = scopes.find(s => s.start <= start && s.end >= end && (!isNextIntlScopeRange(s) || s.functionName === nextIntlFunctionName))
   const quoted = QUOTE_SYMBOLS.includes(text[start - 1])
 
   const namespace = scope?.namespace || defaultNamespace
